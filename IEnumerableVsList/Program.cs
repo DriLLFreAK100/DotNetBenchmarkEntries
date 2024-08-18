8ddc3c7630738fdffd4c388787e7e01f2cd7ae81
drillfreak100 <blusky_8893@hotmail.com>
2024-08-18 23:40:14 +0800

    feat: initial commit with IEnumerable vs List

---
+++ IEnumerableVsList/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Running;
+
+namespace IEnumerableVsList;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    BenchmarkRunner.Run<TestEntry>(
+      DefaultConfig.Instance
+        .AddDiagnoser(
+          MemoryDiagnoser.Default,
+          EventPipeProfiler.Default)
+    );
+  }
+}
+
+[SimpleJob]
+public class TestEntry
+{
+  private IEnumerable<Student> _enrolledStudents = [];
+
+  [Params(10000)]
+  public int N;
+
+  [GlobalSetup]
+  public void Setup()
+  {
+    List<Student> students = [];
+
+    for (var i = 0; i < N; i++)
+    {
+      students.Add(new()
+      {
+        Id = i,
+        Name = $"name {i}",
+        IsEnrolled = i % 2 == 0,
+      });
+    }
+
+    _enrolledStudents = students.Where(x => x.IsEnrolled);
+  }
+
+  [Benchmark(Baseline = true)]
+  public void WithIEnumerable()
+  {
+    var input = _enrolledStudents;
+    Loop(input);
+  }
+
+  [Benchmark]
+  public void WithList()
+  {
+    var input = _enrolledStudents.ToList();
+    Loop(input);
+  }
+
+  private static List<string> Loop(IEnumerable<Student> data)
+  {
+    data.Any(x => x.IsEnrolled);
+    data.First();
+    data.Count();
+    data.Sum(x => x.Id);
+    return data.Select(d => d.Name).ToList();
+  }
+}
+
+public class Student
+{
+  public int Id { get; set; }
+
+  public bool IsEnrolled { get; set; }
+
+  public string Name { get; set; }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# About The Repo
+
+This project is intended for testing and demystifying **patterns** in .NET that might have performance impacts.
+
+`Debunking Misbelieves` shall be the motto of this repo. Instead of following "cults" and common misbelieves, I thought it might be better to just write some benchmarks and let the metrics talk instead.
+
+# Project Structure
+
+No fixed structure at the moment. However, each `benchmark` should be a standalone project at this point in time for simplicity sake.
